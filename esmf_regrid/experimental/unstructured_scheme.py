93b1bc2dac55894c89b8bcdba3574f6c424c16a0
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-02-26 09:56:39 +0000

    lint changes and code tidying

--- esmf_regrid/experimental/unstructured_scheme.py
+++ esmf_regrid/experimental/unstructured_scheme.py
@@ -1,11 +1,13 @@
-"""Provides an iris interface for unstructured regridding"""
+"""Provides an iris interface for unstructured regridding."""
 
 import iris
 from iris.analysis._interpolation import get_xy_coords
+
 # from numpy import ma
 
-from esmf_regrid.experimental.unstructured_regrid import MeshInfo
 from esmf_regrid.esmf_regridder import GridInfo, Regridder
+
+# from esmf_regrid.experimental.unstructured_regrid import MeshInfo
 
 
 def _get_mesh_and_dim(cube):
@@ -28,17 +30,17 @@ def _cube_to_GridInfo(cube):
     pass
 
 
-def _regrid_along_dims(regridder, data, src_dim, mdtol):
-    # Before regridding, data is transposed to a standard form.
-    # This will be done either with something like the following code
-    # or else done within the regridder by specifying args.
-    # new_axes = list(range(len(data.shape)))
-    # new_axes.pop(src_dim)
-    # new_axes.append(src_dim)
-    # data = ma.transpose(data, axes=new_axes)
-
-    result = regridder.regrid(data, mdtol=mdtol)
-    return result
+# def _regrid_along_dims(regridder, data, src_dim, mdtol):
+#     # Before regridding, data is transposed to a standard form.
+#     # This will be done either with something like the following code
+#     # or else done within the regridder by specifying args.
+#     # new_axes = list(range(len(data.shape)))
+#     # new_axes.pop(src_dim)
+#     # new_axes.append(src_dim)
+#     # data = ma.transpose(data, axes=new_axes)
+#
+#     result = regridder.regrid(data, mdtol=mdtol)
+#     return result
 
 
 def _create_cube(data, src_cube, mesh_dim, mesh, grid_x, grid_y):
@@ -86,7 +88,9 @@ def _regrid_unstructured_to_rectilinear__perform(src_cube, regrid_info, mdtol):
 
     # Perform regridding with realised data for the moment. This may be changed
     # in future to handle src_cube.lazy_data.
-    new_data = _regrid_along_dims(src_cube.data, mesh_dim, mdtol)
+    new_data = regridder.regrid(src_cube.data, mdtol=mdtol)
+    # When we want to handle extra dimensions, we may want to do something like:
+    # new_data = _regrid_along_dims(src_cube.data, mesh_dim, mdtol)
 
     new_cube = _create_cube(
         new_data,
@@ -103,20 +107,18 @@ def _regrid_unstructured_to_rectilinear__perform(src_cube, regrid_info, mdtol):
 
 
 def regrid_unstructured_to_rectilinear(src_cube, grid_cube, mdtol=0):
-    """
-    TODO: write docstring.
-    """
+    """TODO: write docstring."""
     regrid_info = _regrid_unstructured_to_rectilinear__prepare(src_cube, grid_cube)
     result = _regrid_unstructured_to_rectilinear__perform(src_cube, regrid_info, mdtol)
     return result
 
 
 class MeshToGridESMFRegridder:
-    """
-    TODO: write docstrings.
-    """
+    """TODO: write docstring."""
 
     def __init__(self, src_mesh_cube, target_grid_cube, mdtol=1):
+        """TODO: write docstring."""
+
         # TODO: Record information about the identity of the mesh. This would
         #  typically be a copy of the mesh, though given the potential size of
         #  the mesh, it may make sense to either retain a reference to the actual
@@ -137,6 +139,8 @@ class MeshToGridESMFRegridder:
         _, _, self.grid_x, self.grid_y, self.regridder = partial_regrid_info
 
     def __call__(self, cube):
+        """TODO: write docstring."""
+
         # TODO: Ensure cube has the same mesh as that of the recorded mesh.
 
         # mesh is probably an iris Mesh object, though it could also be a MeshCoord
