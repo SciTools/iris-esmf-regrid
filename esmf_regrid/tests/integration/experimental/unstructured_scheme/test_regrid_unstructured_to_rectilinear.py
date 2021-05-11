2d92c6c8295061bced5e0d84f95c7ab85492fd2e
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-05-11 16:44:14 +0100

    tidy CI

--- esmf_regrid/tests/integration/experimental/unstructured_scheme/test_regrid_unstructured_to_rectilinear.py
+++ esmf_regrid/tests/integration/experimental/unstructured_scheme/test_regrid_unstructured_to_rectilinear.py
@@ -8,7 +8,7 @@ from iris.experimental.ugrid import PARSE_UGRID_ON_LOAD
 
 def test_real_data():
     test_data_dir = iris.config.TEST_DATA_DIR
-    fn = os.path.join(test_data_dir, "test_data", "NetCDF", "unstructured_grid", "lfric_ngvat_3D_snow_pseudo_levels_1t_face_half_levels_main_snow_layer_temp.nc")
+    fn = os.path.join(test_data_dir, "NetCDF", "unstructured_grid", "lfric_ngvat_3D_snow_pseudo_levels_1t_face_half_levels_main_snow_layer_temp.nc")
     with PARSE_UGRID_ON_LOAD.context():
         cubes = iris.load(fn)
         cube = iris.load_cube(fn, "snow_layer_temperature")
