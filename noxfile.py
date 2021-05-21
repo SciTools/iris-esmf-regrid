44d604896091ceba2070029471c95299e330488a
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2021-05-21 14:43:20 +0100

    Conda lock (#68)
    * Manage dependencies via Conda lock files.
    
    * Conda-lock GHA dependency fixes.
    
    * Treat .cirrus.yml as the source of truth for IRIS_SOURCE.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Address CI failures.
    
    * Rationalised Nox env prep.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove unused nox imports.
    
    * Add pip as dependency - required within Nox session environments.
    
    * Revert "Add pip as dependency" - I was mistaken!
    
    This reverts commit b52bb5ef70af0c33ac563c5d6d07932e0ff536c7.
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -5,11 +5,13 @@ For further details, see https://nox.thea.codes/en/stable/#
 
 """
 
-import hashlib
 import os
 from pathlib import Path
+import shutil
+from urllib.request import urlopen
 
 import nox
+from nox.logger import logger
 import yaml
 
 
@@ -30,68 +32,79 @@ IRIS_SOURCE = os.environ.get("IRIS_SOURCE", None)
 
 COVERAGE_PACKAGES = ["pytest-cov", "codecov"]
 IRIS_GITHUB = "https://github.com/scitools/iris.git"
+LOCKFILE_PLATFORM = "linux-64"
 
 
-def _cache_venv(session, fname):
+def _lockfile_path(py_string: str, platform_placeholder: bool = False) -> Path:
+    """
+    Return a constructed lockfile path for the relevant python string e.g ``py38``.
+
+    Optionally retain the ``{platform}`` placeholder to support conda-lock's
+    ``--filename-template``.
+
+    """
+    dir = Path() / "requirements" / "nox.lock"
+    name_template = "{py_string}-{platform}.lock"
+    if platform_placeholder:
+        platform = "{platform}"
+    else:
+        platform = LOCKFILE_PLATFORM
+    lockfile_name = name_template.format(py_string=py_string, platform=platform)
+    return dir / lockfile_name
+
+
+def _session_lockfile(session: nox.sessions.Session) -> Path:
+    """Return the path of the session lockfile."""
+    return _lockfile_path(py_string=f"py{session.python.replace('.', '')}")
+
+
+def _file_content(file_path: Path) -> str:
+    with file_path.open("r") as file:
+        return file.read()
+
+
+def _session_cachefile(session: nox.sessions.Session) -> Path:
+    """Return the path of the session lockfile cache."""
+    tmp_dir = Path(session.create_tmp())
+    cache = tmp_dir / _session_lockfile(session).name
+    return cache
+
+
+def _venv_populated(session: nox.sessions.Session) -> bool:
+    """Return True if the Conda venv has been created and the list of packages in the lockfile installed."""
+    return _session_cachefile(session).is_file()
+
+
+def _venv_changed(session: nox.sessions.Session) -> bool:
+    """Return True if the installed session is different to that specified in the lockfile."""
+    result = False
+    if _venv_populated(session):
+        expected = _file_content(_session_lockfile(session))
+        actual = _file_content(_session_cachefile(session))
+        result = actual != expected
+    return result
+
+
+def _install_and_cache_venv(session: nox.sessions.Session) -> None:
     """
     Cache the nox session environment.
 
     This consists of saving a hexdigest (sha256) of the associated
-    conda requirements YAML file.
+    Conda lock file.
 
     Parameters
     ----------
     session: object
         A `nox.sessions.Session` object.
-    fname: str
-        Requirements filename that defines this environment.
 
     """
-    python_version = session.python.replace(".", "")
-    with open(fname, "rb") as fi:
-        hexdigest = hashlib.sha256(fi.read()).hexdigest()
-    tmp_dir = Path(session.create_tmp())
-    cache = tmp_dir / f"py{python_version}.sha"
-    with open(cache, "w") as fo:
-        fo.write(hexdigest)
+    lockfile = _session_lockfile(session)
+    session.conda_install(f"--file={lockfile}")
+    with _session_cachefile(session).open("w") as cachefile:
+        cachefile.write(_file_content(lockfile))
 
 
-def _combine_requirements(primary, secondary, ignore=None):
-    """
-    Combine the conda environment YAML files together into one.
-
-    Parameters
-    ----------
-    primary: str
-        The filename of the primary YAML conda environment.
-    secondary: str
-        The filename of the subordinate YAML conda environment.
-    ignore: str, optional
-        The prefix of any package name to be ignored from the
-        combined dependency requirements.
-
-    Returns
-    -------
-    dict
-        A dictionary of the combined YAML conda environments.
-
-    """
-    with open(primary, "r") as fi:
-        result = yaml.load(fi, Loader=yaml.FullLoader)
-    with open(secondary, "r") as fi:
-        secondary = yaml.load(fi, Loader=yaml.FullLoader)
-    # Combine the channels and dependencies only.
-    for key in ["channels", "dependencies"]:
-        result[key] = sorted(set(result[key]).union(secondary[key]))
-    if ignore:
-        # Filter out any specific prefixed package dependencies.
-        result["dependencies"] = [
-            spec for spec in result["dependencies"] if not spec.startswith(ignore)
-        ]
-    return result
-
-
-def _get_iris_github_artifact(session):
+def _get_iris_github_artifact(session: nox.sessions.Session) -> str:
     """
     Determine whether an Iris source artifact from GitHub is required.
 
@@ -109,6 +122,14 @@ def _get_iris_github_artifact(session):
 
     """
     result = IRIS_SOURCE
+    if not result:
+        # .cirrus.yml sets IRIS_SOURCE. Need to fetch the value (if any) when
+        # called outside Cirrus (e.g. user, ASV).
+        # .cirrus.yml = single-source-of-truth.
+        with Path(".cirrus.yml").open("r") as file:
+            cirrus_config = yaml.load(file, Loader=yaml.FullLoader)
+        result = cirrus_config["env"].get("IRIS_SOURCE", None)
+
     # The CLI overrides the environment variable.
     for arg in session.posargs:
         if arg.startswith("--iris="):
@@ -130,34 +151,126 @@ def _get_iris_github_artifact(session):
     return result
 
 
-def _venv_cached(session, fname):
+def _prepare_env(session: nox.sessions.Session) -> None:
+    venv_dir = session.virtualenv.location_name
+
+    if not _venv_populated(session):
+        # Environment has been created but packages not yet installed.
+        # Populate the environment from the lockfile.
+        logger.debug(f"Populating conda env: {venv_dir}")
+        _install_and_cache_venv(session)
+
+    elif _venv_changed(session):
+        # Destroy the environment and rebuild it.
+        logger.debug(f"Lockfile changed. Recreating conda env: {venv_dir}")
+        _reuse_original = session.virtualenv.reuse_existing
+        session.virtualenv.reuse_existing = False
+        session.virtualenv.create()
+        _install_and_cache_venv(session)
+        session.virtualenv.reuse_existing = _reuse_original
+
+    logger.debug(f"Environment up to date: {venv_dir}")
+
+    iris_artifact = _get_iris_github_artifact(session)
+    if iris_artifact:
+        # Install the iris source in develop mode.
+        tmp_dir = Path(session.create_tmp())
+        iris_dir = tmp_dir / "iris"
+        cwd = Path.cwd()
+        if not iris_dir.is_dir():
+            session.run_always(
+                "git", "clone", IRIS_GITHUB, str(iris_dir), external=True
+            )
+        session.cd(str(iris_dir))
+        session.run_always("git", "fetch", "origin", external=True)
+        session.run_always("git", "checkout", iris_artifact, external=True)
+        session.cd(str(cwd))
+        session.install("--no-deps", "--editable", str(iris_dir))
+
+    # Determine whether verbose diagnostics have been requested
+    # from the command line.
+    verbose = "-v" in session.posargs or "--verbose" in session.posargs
+
+    if verbose:
+        session.run_always("conda", "info")
+        session.run_always("conda", "list", f"--prefix={venv_dir}")
+        session.run_always(
+            "conda",
+            "list",
+            f"--prefix={venv_dir}",
+            "--explicit",
+        )
+
+
+@nox.session
+def update_lockfiles(session: nox.sessions.Session):
     """
-    Determine whether the nox session environment has been cached.
+    Re-resolve env specs and store as lockfiles (``requirements/nox.lock/``).
+
+    Original Conda environment specifications are at:
+    ``requirements/py**.yml``. The output lock files denote the dependencies
+    that iris-esmf-regrid is tested against, and therefore officially supports.
 
     Parameters
     ----------
     session: object
-        A `nox.sessions.Session` object.
-    fname: str
-        Requirements filename that defines this environment.
-
-    Returns
-    -------
-    bool
-        Whether the session has been cached.
+        A `nox.sessions.Session object.
 
     """
-    result = False
-    tmp_dir = Path(session.create_tmp())
-    python_version = session.python.replace(".", "")
-    cache = tmp_dir / f"py{python_version}.sha"
-    if cache.is_file():
-        with open(fname, "rb") as fi:
-            hexdigest = hashlib.sha256(fi.read()).hexdigest()
-        with open(cache, "r") as fi:
-            cached = fi.read()
-        result = cached == hexdigest
-    return result
+    session.install("conda-lock")
+
+    for req_file in Path("requirements").glob(r"py[0-9]*.yml"):
+        python_string = req_file.stem
+
+        # Generate the appropriate conda-lock template name, keeping the {platform}
+        # placeholder to support conda-lock's internals.
+        filename_template = _lockfile_path(python_string, platform_placeholder=True)
+        lockfile_path = _lockfile_path(python_string, platform_placeholder=False)
+        # Create the parent directory if it doesn't already exist.
+        try:
+            filename_template.parent.mkdir()
+        except FileExistsError:
+            pass
+
+        # Use a copy of the requirements file in a tmp dir - the file will
+        # be modified if installing a custom Iris checkout.
+        tmp_dir = Path(session.create_tmp())
+        req_file_local = tmp_dir / req_file.name
+        shutil.copy(req_file, req_file_local)
+
+        conda_lock_cmd = [
+            "conda-lock",
+            "lock",
+            f"--filename-template={filename_template}",
+            f"--file={req_file_local}",
+            f"--platform={LOCKFILE_PLATFORM}",
+        ]
+
+        # Get the requirements for Iris too, if an Iris checkout is specified.
+        iris_artifact = _get_iris_github_artifact(session)
+        if iris_artifact:
+            # Remove ``iris`` from dependencies, if present.
+            with req_file_local.open("r+") as file:
+                reqs = yaml.load(file, Loader=yaml.FullLoader)
+                reqs["dependencies"] = [
+                    spec for spec in reqs["dependencies"] if not spec.startswith("iris")
+                ]
+                yaml.dump(reqs, file)
+
+            iris_req_name = f"{python_string}.yml"
+            iris_req_url = (
+                f"https://raw.githubusercontent.com/SciTools/iris/"
+                f"{iris_artifact}/requirements/ci/{iris_req_name}"
+            )
+            iris_req_file = (tmp_dir / iris_req_name).with_stem(f"{python_string}-iris")
+            iris_req = urlopen(iris_req_url).read()
+            with iris_req_file.open("wb") as file:
+                file.write(iris_req)
+            # Conda-lock can resolve multiple requirements files together.
+            conda_lock_cmd.append(f"--file={iris_req_file}")
+
+        session.run(*conda_lock_cmd, silent=True)
+        print(f"Conda lock file created: {lockfile_path}")
 
 
 @nox.session
@@ -208,70 +321,10 @@ def tests(session):
     session: object
         A `nox.sessions.Session` object.
 
-    Notes
-    -----
-    See
-      - https://github.com/theacodes/nox/issues/346
-      - https://github.com/theacodes/nox/issues/260
-
     """
-    artifact = _get_iris_github_artifact(session)
-    python_version = session.python.replace(".", "")
-    requirements_fname = f"requirements/py{python_version}.yml"
-
-    if artifact:
-        tmp_dir = Path(session.create_tmp())
-        artifact_dir = tmp_dir / "iris"
-        cwd = Path.cwd()
-        if not artifact_dir.is_dir():
-            session.run("git", "clone", IRIS_GITHUB, str(artifact_dir), external=True)
-        session.cd(artifact_dir)
-        session.run("git", "fetch", "origin", external=True)
-        session.run("git", "checkout", artifact, external=True)
-        session.cd(str(cwd))
-        iris_requirements_fname = (
-            f"{artifact_dir}/requirements/ci/py{python_version}.yml"
-        )
-        requirements_yaml = _combine_requirements(
-            requirements_fname, iris_requirements_fname, ignore="iris"
-        )
-        requirements_fname = tmp_dir / "requirements.yml"
-        with open(requirements_fname, "w") as fo:
-            yaml.dump(requirements_yaml, fo)
-
-    # Install the package requirements.
-    if not _venv_cached(session, requirements_fname):
-        # Back-door approach to force nox to use "conda env update".
-        command = (
-            "conda",
-            "env",
-            "update",
-            f"--prefix={session.virtualenv.location}",
-            f"--file={requirements_fname}",
-            "--prune",
-        )
-        session._run(*command, silent=True, external="error")
-        _cache_venv(session, requirements_fname)
-
-    if artifact:
-        # Install the iris source in develop mode.
-        session.install("--no-deps", "--editable", str(artifact_dir))
-
+    _prepare_env(session)
     # Install the esmf-regrid source in develop mode.
     session.install("--no-deps", "--editable", ".")
-
-    # Determine whether verbose diagnostics have been requested from the command line.
-    verbose = "-v" in session.posargs or "--verbose" in session.posargs
-
-    if verbose:
-        session.run("conda", "info")
-        session.run("conda", "list", f"--prefix={session.virtualenv.location}")
-        session.run(
-            "conda",
-            "list",
-            f"--prefix={session.virtualenv.location}",
-            "--explicit",
-        )
 
     if COVERAGE:
         # Execute the tests with code coverage.
