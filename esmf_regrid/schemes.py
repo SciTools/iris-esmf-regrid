5dd08e58c59943dff9e908eca477c2f340035d2a
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-05-25 11:49:41 +0100

    flesh out ESMFAreaWeighted, add documentation

--- esmf_regrid/schemes.py
+++ esmf_regrid/schemes.py
@@ -1,4 +1,4 @@
-"""TBD: public module docstring."""
+"""Provides an iris interface for regridding."""
 
 import copy
 
@@ -10,6 +10,8 @@ from esmf_regrid.esmf_regridder import GridInfo, Regridder
 
 __all__ = [
     "ESMFAreaWeighted",
+    "ESMFAreaWeightedRegridder",
+    "regrid_rectilinear_to_rectilinear",
 ]
 
 
@@ -152,22 +154,119 @@ def _regrid_rectilinear_to_rectilinear__perform(src_cube, regrid_info, mdtol):
 
 
 def regrid_rectilinear_to_rectilinear(src_cube, grid_cube, mdtol=0):
+    """
+    Regrid rectilinear cube onto another rectilinear grid.
+
+    Return a new cube with data values calculated using the area weighted
+    mean of data values from the cube src_cube regridded onto the
+    horizontal grid of grid_cube.
+
+    Parameters
+    ----------
+    src_cube : cube
+        An urectilinear instance of iris.cube.Cube that supplies the data,
+        metadata and coordinates.
+    grid_cube : cube
+        A rectilinear instance of iris.cube.Cube that supplies the desired
+        horizontal grid definition.
+    mdtol : float, optional
+        Tolerance of missing data. The value returned in each element of the
+        returned cube's data array will be masked if the fraction of masked
+        data in the overlapping cells of the source cube exceeds mdtol. This
+        fraction is calculated based on the area of masked cells within each
+        target cell. mdtol=0 means no missing data is tolerated while mdtol=1
+        will mean the resulting element will be masked if and only if all the
+        overlapping cells of the source cube are masked. Defaults to 0.
+
+    Returns
+    -------
+    cube
+        A new iris.cube.Cube instance.
+
+    """
     regrid_info = _regrid_rectilinear_to_rectilinear__prepare(src_cube, grid_cube)
     result = _regrid_rectilinear_to_rectilinear__perform(src_cube, regrid_info, mdtol)
     return result
 
 
 class ESMFAreaWeighted:
-    """TBD: public class docstring."""
+    """
+    A scheme which can be recognised by an iris cube's regrid method.
+
+    This class describes an area-weighted regridding scheme for regridding
+    between horizontal grids with separated X and Y coordinates. It uses
+    ESMF to be able to handle grids in different coordinate systems.
+    """
+
+    def __init__(self, mdtol=0):
+        """
+        Area-weighted scheme for regridding between rectilinear grids.
+
+        Parameters
+        ----------
+        mdtol : float
+            Tolerance of missing data. The value returned in each element of
+            the returned array will be masked if the fraction of missing data
+            exceeds mdtol. This fraction is calculated based on the area of
+            masked cells within each target cell. mdtol=0 means no masked
+            data is tolerated while mdtol=1 will mean the resulting element
+            will be masked if and only if all the overlapping elements of the
+            source grid are masked. Defaults to 0.
+
+        """
+        if not (0 <= mdtol <= 1):
+            msg = "Value for mdtol must be in range 0 - 1, got {}."
+            raise ValueError(msg.format(mdtol))
+        self.mdtol = mdtol
+
+    def __repr__(self):
+        return "ESMFAreaWeighted(mdtol={})".format(self.mdtol)
 
     def regridder(self, src_grid, tgt_grid):
-        """TBD: public method docstring."""
-        return ESMFAreaWeightedRegridder(src_grid, tgt_grid)
+        """
+        Creates an area-weighted regridder to perform regridding from the
+        source grid to the target grid.
+
+        Parameters
+        ----------
+        src_grid : cube
+            The cube defining the source grid.
+        tgt_grid : cube
+            The cube defining the target grid.
+
+        Returns
+        -------
+        ESMFAreaWeightedRegridder
+            A callable with the interface:
+                `callable(cube)`
+            where `cube` is a cube with the same grid as `src_grid`
+            that is to be regridded to the grid of `tgt_grid`.
+        """
+        return ESMFAreaWeightedRegridder(src_grid, tgt_grid, mdtol=self.mdtol)
 
 
 class ESMFAreaWeightedRegridder:
+    """Regridder class for unstructured to rectilinear cubes."""
+
     def __init__(self, src_grid, tgt_grid, mdtol=0):
-        # TODO implement esmf regridder as an iris scheme.
+        """
+        Create regridder for conversions between source grid and target grid.
+
+        Parameters
+        ----------
+        src_grid_cube : cube
+            The rectilinear iris cube providing the source grid.
+        target_grid_cube : cube
+            The rectilinear iris cube providing the target grid.
+        mdtol : float, optional
+            Tolerance of missing data. The value returned in each element of
+            the returned array will be masked if the fraction of masked data
+            exceeds mdtol. mdtol=0 means no missing data is tolerated while
+            mdtol=1 will mean the resulting element will be masked if and only
+            if all the contributing elements of data are masked.
+            Defaults to 0.
+
+        """
         if not (0 <= mdtol <= 1):
             msg = "Value for mdtol must be in range 0 - 1, got {}."
             raise ValueError(msg.format(mdtol))
@@ -182,6 +281,25 @@ class ESMFAreaWeightedRegridder:
         self.src_grid = get_xy_dim_coords(src_grid)
 
     def __call__(self, cube):
+        """
+        Regrid this cube onto the target grid of this regridder instance.
+
+        The given cube must be defined with the same grid as the source
+        cube used to create this ESMFAreaWeightedRegridder instance.
+
+        Parameters
+        ----------
+        cube : cube
+            A iris.cube.Cube instance to be regridded.
+
+        Returns
+        -------
+            A cube defined with the horizontal dimensions of the target
+            and the other dimensions from this cube. The data values of
+            this cube will be converted to values on the new grid using
+            area-weighted regridding via ESMF generated weights.
+
+        """
         src_x, src_y = get_xy_dim_coords(cube)
 
         # Check the source grid matches that used in initialisation
