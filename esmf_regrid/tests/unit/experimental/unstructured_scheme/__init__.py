968ef990ab12e3c91210c58fba7bbbfaf1bca914
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-07-12 10:40:42 +0100

    test with multiple iris versions

--- esmf_regrid/tests/unit/experimental/unstructured_scheme/__init__.py
+++ esmf_regrid/tests/unit/experimental/unstructured_scheme/__init__.py
@@ -1,1 +1,8 @@
 """Unit tests for :mod:`esmf_regrid.experimental.unstructured_scheme`."""
+
+try:
+    import iris.experimental.ugrid
+    iris.experimental.ugrid.Mesh
+except:
+    msg = 'skipping tests which use unstructured iris cubes'
+    pytestmark = pytest.mark.skip(msg)
