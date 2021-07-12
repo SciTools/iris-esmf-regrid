3d071581220a145a5142a78b4a70f14d2ae3db9b
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-07-12 12:40:13 +0100

    generate lockfiles

--- noxfile.py
+++ noxfile.py
@@ -35,7 +35,9 @@ IRIS_GITHUB = "https://github.com/scitools/iris.git"
 LOCKFILE_PLATFORM = "linux-64"
 
 
-def _lockfile_path(py_string: str, platform_placeholder: bool = False) -> Path:
+def _lockfile_path(
+    py_string: str, platform_placeholder: bool = False, experimental: bool = False
+) -> Path:
     """
     Return a constructed lockfile path for the relevant python string e.g ``py38``.
 
@@ -44,12 +46,18 @@ def _lockfile_path(py_string: str, platform_placeholder: bool = False) -> Path:
 
     """
     dir = Path() / "requirements" / "nox.lock"
-    name_template = "{py_string}-{platform}.lock"
+    name_template = "{py_string}{experimental}-{platform}.lock"
     if platform_placeholder:
         platform = "{platform}"
     else:
         platform = LOCKFILE_PLATFORM
-    lockfile_name = name_template.format(py_string=py_string, platform=platform)
+    if IRIS_SOURCE is not None or experimental:
+        experimental = "-experimental"
+    else:
+        experimental = ""
+    lockfile_name = name_template.format(
+        py_string=py_string, experimental=experimental, platform=platform
+    )
     return dir / lockfile_name
 
 
@@ -104,7 +112,9 @@ def _install_and_cache_venv(session: nox.sessions.Session) -> None:
         cachefile.write(_file_content(lockfile))
 
 
-def _get_iris_github_artifact(session: nox.sessions.Session) -> str:
+def _get_iris_github_artifact(
+    session: nox.sessions.Session, experimental: bool = False
+) -> str:
     """
     Determine whether an Iris source artifact from GitHub is required.
 
@@ -122,6 +132,10 @@ def _get_iris_github_artifact(session: nox.sessions.Session) -> str:
 
     """
     result = IRIS_SOURCE
+    if experimental:
+        # While we are testing against multiple versions of iris, we have to
+        # define the iris source here also.
+        result = "github:mesh-data-model"
     if not result:
         # .cirrus.yml sets IRIS_SOURCE. Need to fetch the value (if any) when
         # called outside Cirrus (e.g. user, ASV).
@@ -220,57 +234,68 @@ def update_lockfiles(session: nox.sessions.Session):
     session.install("conda-lock")
 
     for req_file in Path("requirements").glob(r"py[0-9]*.yml"):
-        python_string = req_file.stem
+        for experimental in [True, False]:
+            python_string = req_file.stem
 
-        # Generate the appropriate conda-lock template name, keeping the {platform}
-        # placeholder to support conda-lock's internals.
-        filename_template = _lockfile_path(python_string, platform_placeholder=True)
-        lockfile_path = _lockfile_path(python_string, platform_placeholder=False)
-        # Create the parent directory if it doesn't already exist.
-        try:
-            filename_template.parent.mkdir()
-        except FileExistsError:
-            pass
-
-        # Use a copy of the requirements file in a tmp dir - the file will
-        # be modified if installing a custom Iris checkout.
-        tmp_dir = Path(session.create_tmp())
-        req_file_local = tmp_dir / req_file.name
-        shutil.copy(req_file, req_file_local)
-
-        conda_lock_cmd = [
-            "conda-lock",
-            "lock",
-            f"--filename-template={filename_template}",
-            f"--file={req_file_local}",
-            f"--platform={LOCKFILE_PLATFORM}",
-        ]
-
-        # Get the requirements for Iris too, if an Iris checkout is specified.
-        iris_artifact = _get_iris_github_artifact(session)
-        if iris_artifact:
-            # Remove ``iris`` from dependencies, if present.
-            with req_file_local.open("r+") as file:
-                reqs = yaml.load(file, Loader=yaml.FullLoader)
-                reqs["dependencies"] = [
-                    spec for spec in reqs["dependencies"] if not spec.startswith("iris")
-                ]
-                yaml.dump(reqs, file)
-
-            iris_req_name = f"{python_string}.yml"
-            iris_req_url = (
-                f"https://raw.githubusercontent.com/SciTools/iris/"
-                f"{iris_artifact}/requirements/ci/{iris_req_name}"
+            # Generate the appropriate conda-lock template name, keeping the {platform}
+            # placeholder to support conda-lock's internals.
+            filename_template = _lockfile_path(
+                python_string, platform_placeholder=True, experimental=experimental
             )
-            iris_req_file = (tmp_dir / iris_req_name).with_stem(f"{python_string}-iris")
-            iris_req = urlopen(iris_req_url).read()
-            with iris_req_file.open("wb") as file:
-                file.write(iris_req)
-            # Conda-lock can resolve multiple requirements files together.
-            conda_lock_cmd.append(f"--file={iris_req_file}")
+            lockfile_path = _lockfile_path(
+                python_string, platform_placeholder=False, experimental=experimental
+            )
+            # Create the parent directory if it doesn't already exist.
+            try:
+                filename_template.parent.mkdir()
+            except FileExistsError:
+                pass
 
-        session.run(*conda_lock_cmd, silent=True)
-        print(f"Conda lock file created: {lockfile_path}")
+            # Use a copy of the requirements file in a tmp dir - the file will
+            # be modified if installing a custom Iris checkout.
+            tmp_dir = Path(session.create_tmp())
+            req_file_local = tmp_dir / req_file.name
+            shutil.copy(req_file, req_file_local)
+
+            conda_lock_cmd = [
+                "conda-lock",
+                "lock",
+                f"--filename-template={filename_template}",
+                f"--file={req_file_local}",
+                f"--platform={LOCKFILE_PLATFORM}",
+            ]
+
+            # Get the requirements for Iris too, if an Iris checkout is specified.
+            iris_artifact = _get_iris_github_artifact(
+                session, experimental=experimental
+            )
+            if iris_artifact:
+                # Remove ``iris`` from dependencies, if present.
+                with req_file_local.open("r+") as file:
+                    reqs = yaml.load(file, Loader=yaml.FullLoader)
+                    reqs["dependencies"] = [
+                        spec
+                        for spec in reqs["dependencies"]
+                        if not spec.startswith("iris")
+                    ]
+                    yaml.dump(reqs, file)
+
+                iris_req_name = f"{python_string}.yml"
+                iris_req_url = (
+                    f"https://raw.githubusercontent.com/SciTools/iris/"
+                    f"{iris_artifact}/requirements/ci/{iris_req_name}"
+                )
+                iris_req_file = (tmp_dir / iris_req_name).with_stem(
+                    f"{python_string}-iris"
+                )
+                iris_req = urlopen(iris_req_url).read()
+                with iris_req_file.open("wb") as file:
+                    file.write(iris_req)
+                # Conda-lock can resolve multiple requirements files together.
+                conda_lock_cmd.append(f"--file={iris_req_file}")
+
+            session.run(*conda_lock_cmd, silent=True)
+            print(f"Conda lock file created: {lockfile_path}")
 
 
 @nox.session
