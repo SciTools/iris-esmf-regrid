172ef4affa87a456461156eae12a75e3281a536a
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-11-15 13:40:46 +0000

    fix test

--- esmf_regrid/tests/unit/experimental/io/test_save_regridder.py
+++ esmf_regrid/tests/unit/experimental/io/test_save_regridder.py
@@ -5,7 +5,7 @@ import pytest
 from esmf_regrid.experimental.io import save_regridder
 
 
-def test_invalid_type(tmp_file):
+def test_invalid_type(tmp_path):
     """Test that `save_regridder` raises a TypeError where appropriate."""
     invalid_obj = None
     filename = tmp_path / "regridder.nc"
