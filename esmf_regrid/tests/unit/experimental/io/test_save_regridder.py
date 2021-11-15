1e7cee4b441910d13f0f7e63598ab1408ab4f60f
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-11-15 13:31:26 +0000

    refresh nox cache

--- esmf_regrid/tests/unit/experimental/io/test_save_regridder.py
+++ esmf_regrid/tests/unit/experimental/io/test_save_regridder.py
@@ -3,12 +3,11 @@
 import pytest
 
 from esmf_regrid.experimental.io import save_regridder
-from esmf_regrid.tests import temp_filename
 
 
-def test_invalid_type():
+def test_invalid_type(tmp_file):
     """Test that `save_regridder` raises a TypeError where appropriate."""
     invalid_obj = None
-    with temp_filename(suffix=".nc") as filename:
-        with pytest.raises(TypeError):
-            save_regridder(invalid_obj, filename)
+    filename = tmp_path / "regridder.nc"
+    with pytest.raises(TypeError):
+        save_regridder(invalid_obj, filename)
