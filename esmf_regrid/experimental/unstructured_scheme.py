3b987eb3e538e43377a7062e24d1aaf0f5e5caa2
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2022-03-15 11:32:29 +0000

    address review comment

--- esmf_regrid/experimental/unstructured_scheme.py
+++ esmf_regrid/experimental/unstructured_scheme.py
@@ -761,8 +761,8 @@ def regrid_rectilinear_to_unstructured(
         overlapping cells of the ``src_cube`` are masked.
     method : str, default="conservative"
         Either "conservative" or "bilinear". Corresponds to the :mod:`ESMF` methods
-        :attr:`ESMF.RegridMethod.CONSERVE` or :attr:`ESMF.RegridMethod.BILINEAR` used
-        to calculate weights.
+        :attr:`ESMF.api.constants.RegridMethod.CONSERVE` or
+        :attr:`ESMF.api.constants.RegridMethod.BILINEAR` used to calculate weights.
 
     Returns
     -------
@@ -806,8 +806,8 @@ class GridToMeshESMFRegridder:
             for conservative regregridding and 0 for bilinear regridding.
         method : str, default="conservative"
             Either "conservative" or "bilinear". Corresponds to the :mod:`ESMF` methods
-            :attr:`ESMF.RegridMethod.CONSERVE` or :attr:`ESMF.RegridMethod.BILINEAR` used
-            to calculate weights.
+            :attr:`ESMF.api.constants.RegridMethod.CONSERVE` or
+            :attr:`ESMF.api.constants.RegridMethod.BILINEAR` used to calculate weights.
         precomputed_weights : :class:`scipy.sparse.spmatrix`, optional
             If ``None``, :mod:`ESMF` will be used to
             calculate regridding weights. Otherwise, :mod:`ESMF` will be bypassed
