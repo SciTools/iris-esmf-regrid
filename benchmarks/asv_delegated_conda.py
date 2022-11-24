e9fb19f28b1f1dc43d7e676c37d0a0acadffd780
Martin Yeo <martin.yeo@metoffice.gov.uk>
2022-11-24 13:55:07 +0000

    Remove licence header from asv_delegated_conda.py.

--- benchmarks/asv_delegated_conda.py
+++ benchmarks/asv_delegated_conda.py
@@ -1,8 +1,3 @@
-# Copyright Iris contributors
-#
-# This file is part of Iris and is released under the LGPL license.
-# See COPYING and COPYING.LESSER in the root of the repository for full
-# licensing details.
 """
 ASV plug-in providing an alternative :class:`asv.plugins.conda.Conda`
 subclass that manages the Conda environment via custom user scripts.
