3f8c943cde43cd7501e1b128ecb0f0e6b47c08e8
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-02-23 15:34:01 +0000

    fix import

--- esmf_regrid/_esmf_sdo.py
+++ esmf_regrid/_esmf_sdo.py
@@ -3,7 +3,14 @@
 from abc import ABC, abstractmethod
 
 import cartopy.crs as ccrs
-import ESMF
+try:
+    import esmpy
+except ImportError as exc:
+    # Prior to v8.4.0, `esmpy`` could be imported as `ESMF`.
+    try:
+        import ESMF as esmpy  # noqa: N811
+    except ImportError:
+        raise exc
 import numpy as np
 import scipy.sparse
 
