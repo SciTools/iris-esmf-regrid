6f700b6757548f69fe6e2a28658e89f453c1af76
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2023-03-22 12:05:18 +0000

    parameterize test

--- esmf_regrid/tests/unit/experimental/unstructured_scheme/test_GridToMeshESMFRegridder.py
+++ esmf_regrid/tests/unit/experimental/unstructured_scheme/test_GridToMeshESMFRegridder.py
@@ -426,13 +426,20 @@ def test_curvilinear():
     assert result_lazy == result
 
 
-def test_masks():
+@pytest.mark.parametrize(
+    "resolution", (None, 2), ids=("no resolution", "with resolution")
+)
+def test_masks(resolution):
     """
     Test initialisation of :func:`esmf_regrid.experimental.unstructured_scheme.GridToMeshESMFRegridder`.
 
     Checks that the `use_src_mask` and `use_tgt_mask` keywords work properly.
     """
-    src = _curvilinear_cube(7, 6, [-180, 180], [-90, 90])
+    if resolution is None:
+        src = _curvilinear_cube(7, 6, [-180, 180], [-90, 90])
+    else:
+        # The resolution keyword is only valid for rectilinear grids.
+        src = _grid_cube(7, 6, [-180, 180], [-90, 90])
     tgt = _gridlike_mesh_cube(6, 7)
 
     # Make src discontiguous at (0, 0)
@@ -440,52 +447,21 @@ def test_masks():
     src_mask[0, 0] = True
     src.data = np.ma.array(src.data, mask=src_mask)
     src_discontiguous = src.copy()
-    src_discontiguous.coord("latitude").bounds[0, 0] = 0
-    src_discontiguous.coord("longitude").bounds[0, 0] = 0
+    if resolution is None:
+        src_discontiguous.coord("latitude").bounds[0, 0] = 0
+        src_discontiguous.coord("longitude").bounds[0, 0] = 0
 
     tgt_mask = np.zeros([7 * 6], dtype=bool)
     tgt_mask[0] = True
     tgt.data = np.ma.array(tgt.data, mask=tgt_mask)
 
-    rg_src_masked = GridToMeshESMFRegridder(src_discontiguous, tgt, use_src_mask=True)
-    rg_tgt_masked = GridToMeshESMFRegridder(src, tgt, use_tgt_mask=True)
-    rg_unmasked = GridToMeshESMFRegridder(src, tgt)
-
-    weights_src_masked = rg_src_masked.regridder.weight_matrix
-    weights_tgt_masked = rg_tgt_masked.regridder.weight_matrix
-    weights_unmasked = rg_unmasked.regridder.weight_matrix
-
-    # Check there are no weights associated with the masked point.
-    assert weights_src_masked[:, 0].nnz == 0
-    assert weights_tgt_masked[0].nnz == 0
-
-    # Check all other weights are correct.
-    assert np.allclose(
-        weights_src_masked[:, 1:].todense(), weights_unmasked[:, 1:].todense()
+    rg_src_masked = GridToMeshESMFRegridder(
+        src_discontiguous, tgt, use_src_mask=True, resolution=resolution
     )
-    assert np.allclose(weights_tgt_masked[1:].todense(), weights_unmasked[1:].todense())
-
-
-def test_masks_with_resolution():
-    """
-    Test initialisation of :func:`esmf_regrid.experimental.unstructured_scheme.GridToMeshESMFRegridder`.
-
-    Checks that the `use_src_mask` and `use_tgt_mask` keywords work properly.
-    """
-    src = _grid_cube(7, 6, [-180, 180], [-90, 90])
-    tgt = _gridlike_mesh_cube(6, 7)
-
-    src_mask = np.zeros([6, 7], dtype=bool)
-    src_mask[0, 0] = True
-    src.data = np.ma.array(src.data, mask=src_mask)
-
-    tgt_mask = np.zeros([7 * 6], dtype=bool)
-    tgt_mask[0] = True
-    tgt.data = np.ma.array(tgt.data, mask=tgt_mask)
-
-    rg_src_masked = GridToMeshESMFRegridder(src, tgt, use_src_mask=True, resolution=2)
-    rg_tgt_masked = GridToMeshESMFRegridder(src, tgt, use_tgt_mask=True, resolution=2)
-    rg_unmasked = GridToMeshESMFRegridder(src, tgt, resolution=2)
+    rg_tgt_masked = GridToMeshESMFRegridder(
+        src, tgt, use_tgt_mask=True, resolution=resolution
+    )
+    rg_unmasked = GridToMeshESMFRegridder(src, tgt, resolution=resolution)
 
     weights_src_masked = rg_src_masked.regridder.weight_matrix
     weights_tgt_masked = rg_tgt_masked.regridder.weight_matrix
