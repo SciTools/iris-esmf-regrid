db05f6010763c2d82f1f9df120235837821220fc
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2023-04-28 17:06:29 +0100

    add tests

--- esmf_regrid/tests/unit/schemes/test_ESMFAreaWeighted.py
+++ esmf_regrid/tests/unit/schemes/test_ESMFAreaWeighted.py
@@ -5,10 +5,16 @@ from numpy import ma
 import pytest
 
 from esmf_regrid.schemes import ESMFAreaWeighted
+from esmf_regrid.tests.unit.experimental.unstructured_scheme.test__mesh_to_MeshInfo import (
+    _gridlike_mesh_cube,
+)
 from esmf_regrid.tests.unit.schemes.test__cube_to_GridInfo import _grid_cube
 
 
-def test_cube_regrid():
+@pytest.mark.parametrize(
+    "src_type,tgt_type", [("grid", "grid"), ("grid", "mesh"), ("mesh", "grid")]
+)
+def test_cube_regrid(src_type, tgt_type):
     """
     Test that ESMFAreaWeighted can be passed to a cubes regrid method.
 
@@ -23,20 +29,32 @@ def test_cube_regrid():
     n_lats_tgt = 2
     lon_bounds = (-180, 180)
     lat_bounds = (-90, 90)
-    src = _grid_cube(n_lons_src, n_lats_src, lon_bounds, lat_bounds, circular=True)
-    tgt = _grid_cube(n_lons_tgt, n_lats_tgt, lon_bounds, lat_bounds, circular=True)
-    src_data = np.zeros([n_lats_src, n_lons_src])
-    src_mask = np.zeros([n_lats_src, n_lons_src])
-    src_mask[0, 0] = 1
+    if src_type == "grid":
+        src = _grid_cube(n_lons_src, n_lats_src, lon_bounds, lat_bounds, circular=True)
+        src_data = np.zeros([n_lats_src, n_lons_src])
+        src_mask = np.zeros([n_lats_src, n_lons_src])
+        src_mask[0, 0] = 1
+    else:
+        src = _gridlike_mesh_cube(n_lons_src, n_lats_src)
+        src_data = np.zeros([n_lats_src * n_lons_src])
+        src_mask = np.zeros([n_lats_src * n_lons_src])
+        src_mask[0] = 1
+    if tgt_type == "grid":
+        tgt = _grid_cube(n_lons_tgt, n_lats_tgt, lon_bounds, lat_bounds, circular=True)
+        expected_data_default = np.zeros([n_lats_tgt, n_lons_tgt])
+        expected_mask = np.zeros([n_lats_tgt, n_lons_tgt])
+        expected_mask[0, 0] = 1
+    else:
+        tgt = _gridlike_mesh_cube(n_lons_tgt, n_lats_tgt)
+        expected_data_default = np.zeros([n_lats_tgt * n_lons_tgt])
+        expected_mask = np.zeros([n_lats_tgt * n_lons_tgt])
+        expected_mask[0] = 1
     src_data = ma.array(src_data, mask=src_mask)
     src.data = src_data
 
     result_default = src.regrid(tgt, scheme_default)
     result_full = src.regrid(tgt, scheme_full_mdtol)
 
-    expected_data_default = np.zeros([n_lats_tgt, n_lons_tgt])
-    expected_mask = np.zeros([n_lats_tgt, n_lons_tgt])
-    expected_mask[0, 0] = 1
     expected_data_full = ma.array(expected_data_default, mask=expected_mask)
 
     expected_cube_default = tgt.copy()
