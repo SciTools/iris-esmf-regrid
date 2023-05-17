27351d74d427bdce7d9e869015ceaf9140717a0f
Henry Wright <henrywright@sky.com>
2023-05-17 16:40:13 +0100

    24th fix

--- noxfile.py
+++ noxfile.py
@@ -312,7 +312,7 @@ def tests(session: nox.sessions.Session):
     if COVERAGE:
         # Execute the tests with code coverage.
         session.run("pytest", "--cov-report=xml", "--cov")
-        session.run("codecov")#, "--required")
+        session.run("codecov", "--required")
     else:
         # Execute the tests.
         session.run("pytest")
