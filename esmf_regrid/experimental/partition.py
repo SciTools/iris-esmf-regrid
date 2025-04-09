67331e9657c67caafa1c1e6d9f4c6963372ff930
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2025-04-09 14:24:13 +0100

    partial work

--- esmf_regrid/experimental/partition.py
+++ esmf_regrid/experimental/partition.py
@@ -1,8 +1,34 @@
 """Provides an interface for splitting up a large regridding task."""
 
 
+from esmf_regrid.schemes import _ESMFRegridder
+
+class PartialRegridder(_ESMFRegridder):
+    def __init__(self):
+        pass
+
+    ## keep track of source indices and target indices
+    ## share reference to full source and target
 
 class Partition:
-    def __init__(self, src, tgt, scheme, **kwargs):
 
+    ## hold a list of files
+    ## hold a collection of source indices
+    ## note which indices are fully loaded
+    def __init__(self, src, tgt, scheme, file_names, **kwargs):
+        self.src = src
+        self.tgt = tgt
+        self.scheme = scheme
+        self.file_names = file_names
+
+        self.saved_files = []
+
+    @property
+    def unsaved_files(self):
+        return list(set(self.file_names) - set(self.saved_files))
+
+    def generate_files(self, files_to_generate=None):
+        pass
+
+    def apply_regridders(self):
         pass
