9ac8021b042bdab3085a62aa45e8c760976b5bc5
Henry Wright <84939917+HGWright@users.noreply.github.com>
2025-08-01 15:49:08 +0100

    in_dtype is out_dtype for Nearest Regridding (#558)
    * in_dtype is out_dtype for Nearest Regridding
    
    * reformat == with !=
    
    * fix precommit errors
    
    * update failing tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Review comments committed from suggestion
    
    Co-authored-by: Patrick Peglar <patrick.peglar@metoffice.gov.uk>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Patrick Peglar <patrick.peglar@metoffice.gov.uk>

--- src/esmf_regrid/tests/unit/esmf_regridder/test_Regridder.py
+++ src/esmf_regrid/tests/unit/esmf_regridder/test_Regridder.py
@@ -246,7 +246,20 @@ def test_Regridder_dtype_handling():
     src_32 = np.ones([3, 2], dtype=np.float32)
     src_64 = np.ones([3, 2], dtype=np.float64)
 
+    src_int_32 = np.ones([3, 2], dtype=np.int32)
+    src_int_64 = np.ones([3, 2], dtype=np.int64)
+
+    rg_nearest = Regridder(
+        src_grid,
+        tgt_grid,
+        method=Constants.Method.NEAREST,
+        precomputed_weights=_expected_weights(),
+    )
+
     assert rg_64.regrid(src_64).dtype == np.float64
     assert rg_64.regrid(src_32).dtype == np.float64
     assert rg_32.regrid(src_64).dtype == np.float64
     assert rg_32.regrid(src_32).dtype == np.float32
+
+    assert rg_nearest.regrid(src_int_32).dtype == np.int32
+    assert rg_nearest.regrid(src_int_64).dtype == np.int64
