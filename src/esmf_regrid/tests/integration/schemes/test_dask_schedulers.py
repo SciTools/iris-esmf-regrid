1093c939df13e790af32d1f8ab4a625f5be05199
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2026-05-20 16:44:18 +0100

    improve tests

--- src/esmf_regrid/tests/integration/schemes/test_dask_schedulers.py
+++ src/esmf_regrid/tests/integration/schemes/test_dask_schedulers.py
@@ -1,9 +1,42 @@
+"""Integration test for regridding with different schedulers."""
+
 import contextlib
+
 import dask
+import dask.array as da
 import distributed
+from iris.cube import Cube
+import numpy as np
+from esmf_regrid.tests.unit.schemes.test__cube_to_GridInfo import (
+    _grid_cube,
+)
 
 from esmf_regrid.schemes import ESMFAreaWeighted
-from esmf_regrid.tests.unit.schemes import _test_cube_regrid
+
+
+def _test_lazy_regridding():
+    n_lons = 12
+    n_lats = 10
+    h = 4
+    lon_bounds = (-180, 180)
+    lat_bounds = (-90, 90)
+
+    grid = _grid_cube(n_lons, n_lats, lon_bounds, lat_bounds, circular=True)
+    src_data = np.arange(n_lats * n_lons * h, dtype=np.float32).reshape(
+        [n_lats, n_lons, h]
+    )
+    src_data = da.from_array(src_data, chunks=[3, 5, 1])
+    src = Cube(src_data)
+    src.add_dim_coord(grid.coord("latitude"), 0)
+    src.add_dim_coord(grid.coord("longitude"), 1)
+    tgt = _grid_cube(n_lons, n_lats, lon_bounds, lat_bounds, circular=True)
+
+    result = src.regrid(tgt, ESMFAreaWeighted())
+
+    assert result.has_lazy_data()
+    assert result.lazy_data().dtype == np.float64
+    assert result.data.dtype == np.float64
+    assert np.allclose(result.data, src_data)
 
 
 @contextlib.contextmanager
@@ -15,14 +48,14 @@ def distributed_context():
 
 def test_distributed_scheduler():
     with distributed_context():
-        _test_cube_regrid(ESMFAreaWeighted, "grid", "grid")
+        _test_lazy_regridding()
 
 
 def test_processes_scheduler():
     with dask.config.set(scheduler="processes"):
-        _test_cube_regrid(ESMFAreaWeighted, "grid", "grid")
+        _test_lazy_regridding()
 
 
 def test_threads_scheduler():
     with dask.config.set(scheduler="threads"):
-        _test_cube_regrid(ESMFAreaWeighted, "grid", "grid")
+        _test_lazy_regridding()
