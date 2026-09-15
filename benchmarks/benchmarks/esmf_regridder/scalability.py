f9de6fc332092db81c475f4d39f2a4825162058a
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2026-09-15 09:15:42 +0100

    fix license header

--- benchmarks/benchmarks/esmf_regridder/scalability.py
+++ benchmarks/benchmarks/esmf_regridder/scalability.py
@@ -1,6 +1,6 @@
-# Copyright SciTools contributors
+# Copyright Iris-esmf-regrid contributors
 #
-# This file is part of SciTools and is released under the BSD license.
+# This file is part of Iris-esmf-regrid and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
 
 """Scalability benchmarks for :mod:`esmf_regrid.esmf_regridder`."""
